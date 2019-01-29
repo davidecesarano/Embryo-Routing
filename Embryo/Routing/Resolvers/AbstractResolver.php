971240354d1fb8f458a096099d9d1f2e463e0084
davidecesarano <davide.cesarano@unipegaso.it>
2019-01-29 17:47:27 +0100

    Remove ob stream
    Signed-off-by: davidecesarano <davide.cesarano@unipegaso.it>

--- Embryo/Routing/Resolvers/AbstractResolver.php
+++ Embryo/Routing/Resolvers/AbstractResolver.php
@@ -40,50 +40,31 @@
          * @return ResponseInterface
          */
         protected function execute(callable $callable, array $args): ResponseInterface
-        {   
-            ob_start();
-            $level = ob_get_level();
+        { 
+            $return = call_user_func_array($callable, $args);
 
-            try {
-
-                $return = call_user_func_array($callable, $args);
-
-                if ($return instanceof ResponseInterface) {
-                    $response = $return;
-                    $return = '';
-                } elseif (is_null($return) || is_scalar($return) || (is_object($return) && method_exists($return, '__toString'))) {
-                    
-                    $response = (new ResponseFactory)->createResponse(200);
-                    $response = $response->write($return);
-
-                } elseif (is_array($return)) {
-
-                    $response = (new ResponseFactory)->createResponse(200);
-                    $response = $response->withHeader('Content-Type', 'application/json;charset=utf-8');
-                    $response = $response->write(json_encode($return));
-                    return $response;
-                    
-                } else {
-                    throw new \UnexpectedValueException(
-                        'The value returned must be scalar, array or an object with __toString method'
-                    );
-                }
-
-                while (ob_get_level() >= $level) {
-                    $return = ob_get_clean().$return;
-                }
-
-                $body = $response->getBody();
-                if ($return !== '' && $body->isWritable()) {
-                    $body->write($return);
-                }
+            if ($return instanceof ResponseInterface) {
+                
+                $response = $return;
                 return $response;
 
-            } catch (\Exception $exception) {
-                while (ob_get_level() >= $level) {
-                    ob_end_clean();
-                }
-                throw $exception;
+            } elseif (is_null($return) || is_scalar($return) || (is_object($return) && method_exists($return, '__toString'))) {
+                
+                $response = (new ResponseFactory)->createResponse(200);
+                $response = $response->write($return);
+                return $response;
+
+            } elseif (is_array($return)) {
+
+                $response = (new ResponseFactory)->createResponse(200);
+                $response = $response->withHeader('Content-Type', 'application/json;charset=utf-8');
+                $response = $response->write(json_encode($return));
+                return $response;
+                
+            } else {
+                throw new \UnexpectedValueException(
+                    'The value returned must be scalar, array or an object with __toString method'
+                );
             }
         }
     }
